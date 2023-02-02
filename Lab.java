04cc47a6318bb9567550b6d7c27dd4899813943c
Alexander Holmes <holm0253@algonquinlive.com>
2023-02-02 16:27:54 -0500

    Git commit

--- Lab.java
+++ Lab.java
@@ -17,19 +17,24 @@ public class Lab {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello World Java Swing");
-		JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
+		//JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
+		JPanel buttonPanel = new JPanel();
 		frame.setBackground(Color.white);
 		//Border line = BorderFactory.createLineBorder(Color.black);
 		frame.setMinimumSize(new Dimension(600,600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JLabel topText = new JLabel("Hello World", SwingConstants.LEADING);
-		JButton areaButtons[] = new JButton[25];
+		JButton areaButtons[] = new JButton[16];
 		topText.setForeground(Color.black);
 		topText.setBorder(new EmptyBorder(0, 300, 50, 50));
 		topText.setOpaque(true);
-		for (int i = 0; i < 25; i++) {//AlexH741/JAPLab1.2
-			areaButtons[i] = new JButton();
-			areaButtons[i].setPreferredSize(new Dimension(25,25));
+		for (int i = 0; i < 16; i++) {
+			areaButtons[i] = createButton(i* 25, i*25, 50, 50);//addActionListener, addMouseListener
+			areaButtons[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+				}
+			 });
 			//areaButtons[i].setBorder(new EmptyBorder(0, 0, i * 25, 25));
 			buttonPanel.add(areaButtons[i]);
 		}
@@ -37,6 +42,11 @@ public class Lab {
 		frame.pack();
 		frame.setVisible(true);
 	}
+
+	public static JButton createButton(int x, int y, int w, int h) {
+		JButton button = new JButton();
+		button.setPreferredSize(new Dimension(w, h));
+		return button;
+	}
 }
 
-//ahihihihih
