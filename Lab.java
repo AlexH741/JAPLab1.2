e74e4a0a635a4e2fdbb2b045a8f5c94576fe0662
AlexH741 <holm0253@algonquinlive.com>
2023-02-04 21:57:54 -0500

    Improved Layout

--- Lab.java
+++ Lab.java
@@ -1,39 +1,50 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
 
 public class Lab {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello World Java Swing");
-		JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
 		frame.setBackground(Color.white);
-		//Border line = BorderFactory.createLineBorder(Color.black);
-		frame.setMinimumSize(new Dimension(600,600));
+		frame.setMinimumSize(new Dimension(800,800));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel topText = new JLabel("Hello World", SwingConstants.LEADING);
-		JButton areaButtons[] = new JButton[25];
-		topText.setForeground(Color.black);
-		topText.setBorder(new EmptyBorder(0, 300, 50, 50));
-		topText.setOpaque(true);
-		for (int i = 0; i < 25; i++) {//AlexH741/JAPLab1.2
+
+		JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
+		JPanel textRightPanel = new JPanel();
+		JPanel textLeftPanel = new JPanel();
+		JPanel textTopPanel = new JPanel();
+
+		JTextArea textArea1 = new JTextArea(":)", 30, 10);
+		textRightPanel.add(textArea1);
+
+		JTextArea textArea2 = new JTextArea(":)", 30, 10);
+		textLeftPanel.add(textArea2);
+
+		JTextArea textArea3 = new JTextArea(":)", 7, 30);
+		textTopPanel.add(textArea3);
+
+		JButton areaButtons[] = new JButton[16];
+
+		for (int i = 0; i < 16; i++) {
 			areaButtons[i] = new JButton();
-			areaButtons[i].setPreferredSize(new Dimension(25,25));
-			//areaButtons[i].setBorder(new EmptyBorder(0, 0, i * 25, 25));
 			buttonPanel.add(areaButtons[i]);
 		}
-		frame.add(buttonPanel);
+
+		frame.add(BorderLayout.PAGE_START, textTopPanel);
+		frame.add(BorderLayout.LINE_END, textRightPanel);
+		frame.add(BorderLayout.LINE_START, textLeftPanel);
+		frame.add(BorderLayout.CENTER, buttonPanel);
 		frame.pack();
 		frame.setVisible(true);
 	}
