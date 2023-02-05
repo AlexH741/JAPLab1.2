6dda2e74d134c84bb82e335faa1660448a6f7f09
AlexH741 <holm0253@algonquinlive.com>
2023-02-04 23:00:03 -0500

    added TODOs

--- Lab.java
+++ Lab.java
@@ -12,7 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.*;
 
-public class Lab {
+public class Lab { //TODO: Javadoc
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello World Java Swing");
@@ -23,9 +23,13 @@ public class Lab {
 		JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
 		JPanel textRightPanel = new JPanel();
 		JPanel textLeftPanel = new JPanel();
-		JPanel textTopPanel = new JPanel();
+		JPanel textTopPanel = new JPanel(); //TODO: add "Mark" button
+		//TODO: add bottom pane?
+		
+		//TODO: add hint texts to panes
+		//TODO: add language change functionality
 
-		JTextArea textArea1 = new JTextArea(":)", 30, 10);
+		JTextArea textArea1 = new JTextArea(":)", 30, 10); //TODO: Change all text areas to JTextPanes or JEditorPanes
 		textRightPanel.add(textArea1);
 
 		JTextArea textArea2 = new JTextArea(":)", 30, 10);
@@ -37,8 +41,8 @@ public class Lab {
 		JButton areaButtons[] = new JButton[16];
 
 		for (int i = 0; i < 16; i++) {
-			areaButtons[i] = new JButton();
-			buttonPanel.add(areaButtons[i]);
+			areaButtons[i] = new JButton(); //TODO: Add event listeners
+			buttonPanel.add(areaButtons[i]); //TODO: Recolor/reskin buttons
 		}
 
 		frame.add(BorderLayout.PAGE_START, textTopPanel);
@@ -49,5 +53,3 @@ public class Lab {
 		frame.setVisible(true);
 	}
 }
-
-//ahihihihih
