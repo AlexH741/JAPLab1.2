f7517941d894068bed46a20c47200fe50e15ea60
AlexH741 <holm0253@algonquinlive.com>
2023-02-05 19:49:21 -0500

    Fixed hint numbers

--- UI.java
+++ UI.java
@@ -12,17 +12,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+
 import javax.swing.*;
 
 public class UI extends JFrame{
-    private JTextArea Area1 = new JTextArea(":)", 30, 10);
+    private JTextArea Area1 = new JTextArea(30, 10);
+    private JButton[][] areaButtons = new JButton[4][4];
 
     public UI() {
         JFrame frame = new JFrame("Picross");
 	    frame.setBackground(Color.white);
 	    frame.setMinimumSize(new Dimension(900,800));
 	    frame.setResizable(false);
-	    //frame.setResizable(isDefaultLookAndFeelDecorated());
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel buttonPanel = new JPanel(new GridLayout(4,4,4,4));
@@ -39,15 +41,11 @@ public class UI extends JFrame{
     }
 
     private JPanel createLeftPanel(JPanel left) {
-        JTextArea Area2 = new JTextArea(":)", 30, 10);  
+        String text = "\n\n\n\n\t (1,1) \n\n\n\n\n\n\n\n\n\n\t (2,2) \n\n\n\n\n\n\n\n\n\n\n\t (3, 3) \n\n\n\n\n\n\n\n\n\n\t (4,4)";
+        JTextArea Area2 = new JTextArea(text , 40, 10);
+        Area2.setEditable(false);  
 		left.add(Area2);
 
-        int[] leftHints = {1, 2, 3, 4};
-		JLabel[] leftLabels = new JLabel[leftHints.length];
-		for (int i = 0; i < leftHints.length; i++) {
-    		leftLabels[i] = new JLabel(String.valueOf(leftHints[i]));
-    		left.add(leftLabels[i]);
-		}
         return left;
     }
 
@@ -64,10 +62,17 @@ public class UI extends JFrame{
         PointsBox.setEditable(false);
 
         JButton resetButton = new JButton("Reset");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Area1.setText(Area1.getText() + "\n Reset game");
+            }
+        });
 
 
 		JScrollPane textArea1 = new JScrollPane(Area1);  
 		textArea1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        Area1.setEditable(false);
 
 
         c.fill = GridBagConstraints.BOTH;
@@ -80,56 +85,42 @@ public class UI extends JFrame{
         c.weightx = 0;
 		right.add(textArea1, c);
 
-        c.fill = GridBagConstraints.RELATIVE;;
+        c.fill = GridBagConstraints.RELATIVE;
         c.gridy = 2;
         right.add(pointsLabel, c);
 
         c.gridwidth = GridBagConstraints.REMAINDER;
         c.gridx = 1;
-        //c.fill = GridBagConstraints.HORIZONTAL;
-        //c.weightx = 0;
-        //.gridx = GridBagConstraints.RELATIVE;
-        //c.gridy = 2;
-        //c.gridwidth = GridBagConstraints.REMAINDER;
-        //c.gridwidth = GridBagConstraints.RELATIVE;
         right.add(PointsBox, c);
 
-        //c.fill = GridBagConstraints.HORIZONTAL;
-        //c.weightx = 0.5;
-        //c.gridx = 0;
-        //c.gridy = 3;
-        //right.add(resetButton, c);
+        c.gridy = 3;
+        right.add(resetButton, c);
 
         return right;
     }
 
     private JPanel createTopPanel(JPanel top) {
         String[] Languages = {"French" , "English", "Vietnamese"};
+        String text = "\n\n\n\n\n\n\n                        (1,1)\t\t          (2,2)\t\t               (3,3)                                          (4,4)";
 
         JCheckBox markButton = new JCheckBox("Mark");
-		top.add(markButton);
 
         JComboBox<Object> LanguageBox = new JComboBox<Object>(Languages);
 		LanguageBox.addActionListener(LanguageBox);
 		LanguageBox.setSelectedIndex(1);
+
+        JTextArea textArea3 = new JTextArea(text, 7, 70);
+        textArea3.setEditable(false);
+        
+		top.add(markButton);
+        top.add(textArea3);
         top.add(LanguageBox);
-
-        JTextArea textArea3 = new JTextArea(":)", 7, 30);
-		top.add(textArea3);
-
-        int[] topHints = {1, 2, 3, 4};
-		JLabel[] topLabels = new JLabel[topHints.length];
-		for (int i = 0; i < topHints.length; i++) {
-    		topLabels[i] = new JLabel(String.valueOf(topHints[i]));
-    		top.add(topLabels[i]);
-		}
 
         return top;
     }
 
     private JPanel createButtons(JPanel buttons) {
-        JButton[][] areaButtons = new JButton[4][4];
-		Boolean[][] markButtons = new Boolean[4][4];
+		//Boolean[][] markButtons = new Boolean[4][4];
 
 		for (int i = 0; i < 4; i++) {
 			final int final_i = i;
@@ -137,11 +128,10 @@ public class UI extends JFrame{
 				final int final_j = j;
 				areaButtons[i][j] = new JButton();
                 areaButtons[i][j].setBackground(new Color(0x7CCD7C));
-
 				areaButtons[i][j].addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						Area1.setText(Area1.getText() + "\n Position " + (final_i + 1) + ", " + (final_j + 1) + " pressed");
+						Area1.setText(Area1.getText() + "\n Pos " + (final_i + 1) + ", " + (final_j + 1));
 					}
 				});
 
@@ -149,5 +139,9 @@ public class UI extends JFrame{
 			}
 		}
         return buttons;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
