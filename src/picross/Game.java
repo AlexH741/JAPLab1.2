06ff3342a4ff850fd372125b8cb61607263bdeab
AlexH741 <holm0253@algonquinlive.com>
2023-03-13 20:32:34 -0400

    re-added game.java

--- src/picross/Game.java
+++ src/picross/Game.java
@@ -1,22 +1,22 @@
 package src.picross;
+
 /**
  * Rough draft of Picross game UI
  * 
  * CET - CS Academic Level 4
  * Student Names: Bich Khe Hoang, Alex Holmes
- * Student Numbers: 040990843 , 041026437
+ * Student Numbers: , 041026437
  * Section 302
  * Course: CST8221 - Java Application Programming
- * Lab Professor: Daniel Cormier
+ * Lab Professor: 
  */
 
-public class Game{
+public class Game{ //TODO: Javadoc
 
-	/**
-	* The main method of the Game class display the UI for the game
-    * @param args The command line arguments.
-	*/
 	public static void main(String[] args) {
+		//Model gameModel = new Model();
+		//View gameView = new View();
+		//Controller gameController = new Controller(gameModel, gameView);
 		UI newUI = new UI();
 	}
 }
